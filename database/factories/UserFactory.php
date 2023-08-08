13d871291bead46fd2a6b7fb7f333cf7b7359945
alirfanyasin <risayunita21@gmail.com>
2023-08-08 21:02:36 +0700

    feat: data dummy

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,22 +17,19 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $firsname = fake()->name();
+        $lastname = fake()->name();
+
         return [
-            'name' => fake()->name(),
+            'name' => $firsname . " " . $lastname,
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'firstname' => $firsname,
+            'lastname' => $lastname,
         ];
     }
 
     /**
      * Indicate that the model's email address should be unverified.
      */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
-    }
 }
