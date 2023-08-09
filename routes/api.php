642f6d04d0aa9b16e4c415094ec7ba3ed4ee0504
alirfanyasin <risayunita21@gmail.com>
2023-08-09 14:32:35 +0700

    feat: update route api file

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,52 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::get('/post', function () {
+    return Post::all();
+    return response()->json(['message' => 'Berhasil Get Data']);
+});
+
+
+
+Route::get('/post/{id}', function ($id) {
+    return Post::find($id);
+    return response()->json(['message' => 'Berhasil Detail Data']);
+});
+
+
+
+Route::post('/post/store', function (Request $request) {
+    $data = [
+        'title' => $request->title,
+        'news_content' => $request->news_content,
+        'author' => $request->author,
+    ];
+
+    Post::create($data);
+    return response()->json(['message' => 'Berhasil Tambah Data']);
+});
+
+
+
+Route::patch('/post/update/{id}', function ($id, Request $request) {
+    $post = Post::find($id);
+
+    if (!$post) {
+        return response()->json(['message' => 'Post not found'], 404);
+    }
+
+    $data = [
+        'title' => $request->input('title'),
+        'news_content' => $request->input('news_content'),
+        'author' => $request->input('author'),
+    ];
+
+    if ($post->update($data)) {
+        return response()->json(['message' => 'Berhasil Update Data']);
+    } else {
+        return response()->json(['message' => 'Gagal Update Data'], 500);
+    }
 });
