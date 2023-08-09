a5417635bed9944fcdefb9d03b42eb3df7653550
alirfanyasin <risayunita21@gmail.com>
2023-08-10 04:18:19 +0700

    feat: Using API Resource custom

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -19,7 +19,8 @@ class PostResource extends JsonResource
         return [
             'id' => $this->id,
             'title' => $this->title,
-            'terserah' => 'Data Terserah tidak ada dalam database'
+            'news_content' => $this->news_content,
+            'created_at' => date_format($this->created_at, 'Y/m/d H:i:s')
         ];
 
 
