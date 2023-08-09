58b66b64f6dfb3209ca17099c66d6ec3aa85b045
alirfanyasin <risayunita21@gmail.com>
2023-08-09 15:35:55 +0700

    feat: integrated controller to route

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        $data = Post::all();
+        return response()->json(['message' => 'Berhasil Get Data', 'data' => $data]);
+    }
+
+
+    public function show($id)
+    {
+        $data = Post::find($id);
+        return response()->json(['message' => 'Berhasil Detail Data', 'data' => $data]);
+    }
+
+    public function store(Request $request)
+    {
+        $data  = $request->all();
+        // $data = [
+        //     'title' => $request->title,
+        //     'news_content' => $request->news_content,
+        //     'author' => $request->author,
+        // ];
+
+        Post::create($data);
+        return response()->json(['message' => 'Berhasil Tambah Data']);
+    }
+
+
+    public function update($id, Request $request)
+    {
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\Post;
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,54 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 
-Route::get('/post', function () {
-    return Post::all();
-    return response()->json(['message' => 'Berhasil Get Data']);
-});
+Route::get('/post', [PostController::class, 'index']);
+Route::get('/post/{id}', [PostController::class, 'show']);
+Route::post('/post/store', [PostController::class, 'store']);
 
-
-
-Route::get('/post/{id}', function ($id) {
-    return Post::find($id);
-    return response()->json(['message' => 'Berhasil Detail Data']);
-});
-
-
-
-Route::post('/post/store', function (Request $request) {
-    $data = [
-        'title' => $request->title,
-        'news_content' => $request->news_content,
-        'author' => $request->author,
-    ];
-
-    Post::create($data);
-    return response()->json(['message' => 'Berhasil Tambah Data']);
-});
-
-
-
-Route::patch('/post/update/{id}', function ($id, Request $request) {
-    $post = Post::find($id);
-
-    if (!$post) {
-        return response()->json(['message' => 'Post not found'], 404);
-    }
-
-    $data = [
-        'title' => $request->input('title'),
-        'news_content' => $request->input('news_content'),
-        'author' => $request->input('author'),
-    ];
-
-    if ($post->update($data)) {
-        return response()->json(['message' => 'Berhasil Update Data']);
-    } else {
-        return response()->json(['message' => 'Gagal Update Data'], 500);
-    }
-});
+Route::patch('/post/update/{id}', [PostController::class, 'update']);
